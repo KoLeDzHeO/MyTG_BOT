171587bd6cec549c9dae68155d3f6b5443adf387
KoLeDzHeO <80348407+KoLeDzHeO@users.noreply.github.com>
2025-09-05 17:12:49 +0200

    Update help.py

--- src/handlers/help.py
+++ src/handlers/help.py
@@ -10,4 +10,3 @@ async def help_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         return
     lang = update.effective_user.language_code or config.LANG_FALLBACKS[0]
     await update.message.reply_text(t("help_text", lang=lang))
-
